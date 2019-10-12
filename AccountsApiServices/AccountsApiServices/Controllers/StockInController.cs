4703c70b028713daeff39d2a55e23f30b7eb9672
Manish <manishmanjrekar@outlook.com>
2019-10-12 16:34:02 +0530

    stockins controller added.

---
+++ AccountsApiServices/AccountsApiServices/Controllers/StockInController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AccountsApiServices.Controllers
+{
+    public class StockInController : ApiController
+    {
+        // GET: api/StockIn
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/StockIn/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/StockIn
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/StockIn/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/StockIn/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
