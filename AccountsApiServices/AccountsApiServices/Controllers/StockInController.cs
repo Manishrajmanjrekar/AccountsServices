19cd92156592d81c080a2ecc6a1890e5778f0049
Manish <manishmanjrekar@outlook.com>
2019-10-12 16:34:52 +0530

    changes

--- AccountsApiServices/AccountsApiServices/Controllers/StockInController.cs
+++ AccountsApiServices/AccountsApiServices/Controllers/StockInController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AccountsApiServices.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -19,6 +20,40 @@ namespace AccountsApiServices.Controllers
         public string Get(int id)
         {
             return "value";
+        }
+
+       [Route("api/StockIn/GetStockInCount")]
+       [HttpPost]
+        public int GetStockInCount([FromBody]string id,string name)
+        {
+            var list = new List<StockIn>()
+            {
+                new StockIn{ Id=1,IsActive=true,NickName="patnam1",TotalQuantity=101,
+                    CreatedDate =DateTime.Now.ToShortDateString(), VendorId =1},
+                 new StockIn{ Id=1,IsActive=true,NickName="patnam2",TotalQuantity=107,
+                    CreatedDate =DateTime.Now.ToShortDateString(), VendorId =1},
+            };
+
+
+            var obj = list.Where(e => e.NickName.Equals(name)).ToList().Count;
+            return obj;
+        }
+
+        [Route("api/StockIn/GetStockForToday")]
+        [HttpGet]
+        public List<StockIn> GetStockForToday()
+        {
+            var list = new List<StockIn>()
+            {
+                new StockIn{ Id=1,IsActive=true,NickName="patnam1",TotalQuantity=101,
+                    CreatedDate =DateTime.Now.ToShortDateString(), VendorId =1},
+                 new StockIn{ Id=2,IsActive=true,NickName="patnam2",TotalQuantity=107,
+                    CreatedDate =DateTime.Now.ToShortDateString(), VendorId =2},
+            };
+
+
+         
+            return list;
         }
 
         // POST: api/StockIn
