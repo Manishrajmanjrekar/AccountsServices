cd874739181d6375b5cec70a7a10b3b93e845988
Manish <manishmanjrekar@outlook.com>
2019-11-16 14:48:29 +0530

    added

---
+++ AccountsApiServices/ApiCoreServices/Controllers/StockInController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiCoreServices.Models;
+using ApiCoreServices.ServicesModels;
+using ApiCoreServices.SqlLayerInterfaces;
+using ApiCoreServices.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCoreServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockInController : ControllerBase
+    {
+        IStockInRepository _StockInRepository;
+
+
+        public StockInController(IStockInRepository StockInRepository)
+        {
+            _StockInRepository = StockInRepository;
+        }
+        // GET: api/Customer
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+
+
+        [HttpPost]
+        [Route("StockById")]
+        public SalesViewModel StockById(string id)
+        {
+           return _StockInRepository.StockById(id);
+        }
+
+        [Route("LoadNames")]
+        [HttpPost]
+        public List<SalesViewModel> LoadNames(AutoCompleteModel data)
+        {
+            return _StockInRepository.LoadNames(data);
+        }
+
+
+        [HttpPost]
+        [Route("StockInCount")]
+        public int StockInCount(StockInViewModel stock)
+        {
+            return _StockInRepository.StockInCount(stock);
+        }
+
+        [HttpPost]
+        [Route("AddStock")]
+        public StockInViewModel AddStock(StockInViewModel data)
+        {
+            return _StockInRepository.AddStock(data);
+        }
+
+
+
+        [HttpPost]
+        [Route("AllVendors")]
+        public List<StockInViewModel> GetAllStockList()
+        {
+            return _StockInRepository.GetAllStockList();
+        }
+
+        [Route("CheckIsDuplicateNickName")]
+        [HttpPost]
+        public bool CheckIsDuplicateNickName([FromBody]string data)
+        {
+            bool isDuplicateNickName = false;
+
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                throw new Exception("name cannot be empty exception");
+            }
+           
+            if ((_StockInRepository.CheckIsDuplicateNickName(data)))
+            {
+                isDuplicateNickName = true;
+            }
+
+            return isDuplicateNickName;
+        }
+
+        [HttpPost]
+        [Route("DeleteCustomerById")]
+        public bool DeleteCustomerById(int id)
+        {
+           var results = _StockInRepository.DeleteVendorById(id);
+           return results;
+        }
+
+
+    }
+}
---
+++ AccountsApiServices/ApiCoreServices/Controllers/VendorController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiCoreServices.Models;
+using ApiCoreServices.SqlLayerInterfaces;
+using ApiCoreServices.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCoreServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        IVendorRepository _VendorRepository;
+
+
+        public VendorController(IVendorRepository VendorRepository)
+        {
+            _VendorRepository = VendorRepository;
+        }
+        // GET: api/Customer
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+
+
+        [HttpPost]
+        [Route("SaveVendor")]
+        public CommonResponseViewModel SaveVendor(VendorViewModel vendorVM)
+        {
+            var response = _VendorRepository.SaveVendor(vendorVM);
+            Console.WriteLine();
+            return null;
+        }
+
+        [HttpPost]
+        [Route("VendorById")]
+        public VendorViewModel VendorById(int id)
+        {
+            VendorViewModel Vendorvm = null;
+            Vendorvm = _VendorRepository.GetVendorById(id);
+            return Vendorvm;
+        }
+
+        [Route("VendorNames")]
+        [HttpPost]
+        public List<VendorViewModel> VendorNames(AutoCompleteModel data)
+        {
+            return _VendorRepository.VendorNames(data);
+        }
+
+
+
+        [HttpPost]
+        [Route("AllVendors")]
+        public List<VendorViewModel> AllVendors()
+        {
+            return _VendorRepository.GetAllVendors();
+        }
+
+        [Route("CheckIsDuplicateNickName")]
+        [HttpPost]
+        public bool CheckIsDuplicateNickName([FromBody]string data)
+        {
+            bool isDuplicateNickName = false;
+
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                throw new Exception("name cannot be empty exception");
+            }
+           
+            if ((_VendorRepository.CheckIsDuplicateNickName(data)))
+            {
+                isDuplicateNickName = true;
+            }
+
+            return isDuplicateNickName;
+        }
+
+        [HttpPost]
+        [Route("DeleteCustomerById")]
+        public bool DeleteCustomerById(int id)
+        {
+           var results = _VendorRepository.DeleteVendorById(id);
+           return results;
+        }
+
+
+    }
+}
