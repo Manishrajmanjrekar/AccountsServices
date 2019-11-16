7d7c1f7f33aebf537a4d82c62649f7f98daa7ed6
Manish <manishmanjrekar@outlook.com>
2019-11-16 15:44:00 +0530

    servicemodels

---
+++ AccountsApiServices/ApiCoreServices/ServicesModels/VendorPaymentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCoreServices.ServicesModels
+{
+    public class VendorPaymentViewModel
+    {
+        public int Id { get; set; }
+        public int VendorId { get; set; }
+        public string VendorName { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int StockInId { get; set; }
+       
+        public int AmountPaid { get; set; }
+        public string CreatedDate { get; set; }
+    }
+}
